ae088e468aa0617750b451afcad790efaa60fa51
zhong <zhong@123.com>
2025-10-04 23:49:56 +0800

    feature: 读取配置文件,启动main

---
+++ exchange_app_backend/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+type Config struct {
+	App struct {
+		Name string
+		Port string
+	}
+	Database struct {
+		Dsn          string
+		MaxIdleConns int
+		MaxOpenConns int
+	}
+	Redis struct {
+		Addr     string
+		DB       int
+		Password string
+	}
+}
+
+var AppConfig *Config
+
+func InitConfig() {
+	viper.SetConfigName("config") // 配置文件名 (不带后缀)
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("./config") // 在 config 子目录查找
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("read config file failed, err:%v\n", err)
+	}
+
+	AppConfig = &Config{}
+	// 将读取到的配置反序列化到 Conf 结构体中
+	if err := viper.Unmarshal(AppConfig); err != nil {
+		log.Fatalf("unmarshal config failed, err:%v\n", err)
+	}
+
+	// 监控配置文件变化并热加载
+	viper.WatchConfig()
+}
---
+++ exchange_app_backend/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"exchange_app/config"
+)
+
+func main() {
+	config.InitConfig()
+}
