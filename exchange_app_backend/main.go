8b1392e9d9b4a50ddfc26c8859f0776dd7385345
zhong <zhong@123.com>
2025-10-05 11:44:23 +0800

    feature: gin初体验

--- exchange_app_backend/main.go
+++ exchange_app_backend/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"exchange_app/config"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run(config.AppConfig.App.Port)
+	if err != nil {
+		return
+	}
 }
